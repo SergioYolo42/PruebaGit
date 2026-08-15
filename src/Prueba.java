a94a90713209c885a70d14ca4ad60972ac17f4db
Cholo <sergioasensisanchez2002@gmail.com>
2026-08-15 16:33:23 +0200

    Pues mira prueba 2

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,5 +1,9 @@
 public class Prueba {
     public static void main(String[] args) {
-
+        int i=0;
+        while(i<100){
+            System.out.println(i);
+            i++;
+        }
     }
 }
